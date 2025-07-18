8012c160ed1f4ad2183b2aa03091dafe3c5e5ada
a.ivashkin <aisek72@gmail.com>
2025-07-18 23:38:28 +0300

    fix: correct calc isTodayEnabled

--- src/utils/isTodayEnabled.utils.ts
+++ src/utils/isTodayEnabled.utils.ts
@@ -3,8 +3,7 @@ import { IDays } from '../store/sleep.store';
 type WeekdayType = 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday';
 
 export default function isTodayEnabled(days: IDays, utcOffset: number): boolean {
-  const nowUtc = new Date();
-  const utcTime = nowUtc.getTime() + utcOffset * 60 * 1000;
+  const utcTime = new Date().getTime() + 4 * 60 * 60 * 1000;
   const adjustedDate = new Date(utcTime);
 
   const weekday = adjustedDate
